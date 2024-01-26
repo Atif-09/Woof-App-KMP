ea431b72bf833e39619f2639585f91d7a1167aa5
Abdul Basit <abdulbasit5361234@gmail.com>
2024-01-26 12:02:04 +0500

    added sticky header

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,4 +1,3 @@
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
@@ -13,6 +12,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,13 +20,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
-import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -37,12 +33,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.materialIcon
 import androidx.compose.material.icons.materialPath
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -50,16 +44,14 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
+import kotlin.math.*
+
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -81,6 +73,140 @@ fun App() {
 
     }
 }
+
+/*@Composable
+fun MainMenuCanvas() {
+    var showSubMenu by remember { mutableStateOf(false) }
+    val mainCircleRadius = 50.dp
+    val subCircleRadius = 20.dp
+    val distanceFromCenter = 100.dp
+    val subCircles = remember { List(6) { mutableStateOf(Offset.Zero) } }
+    val density = LocalDensity.current
+
+    val animationProgress by animateFloatAsState(
+        targetValue = if (showSubMenu) 1f else 0f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 2000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ), label = ""
+    )
+
+    val circleColors = remember {
+        List(6) {
+            Brush.linearGradient(
+                colors = listOf(
+                    Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f),
+                    Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f)
+                )
+            )
+        }
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+    ) {
+        val center = remember { mutableStateOf(Offset.Zero) }
+
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            center.value = Offset(size.width / 2, size.height / 2)
+
+            drawCircle(color = Color.Blue, radius = mainCircleRadius.toPx(), center = center.value)
+
+            if (showSubMenu) {
+                subCircles.forEachIndexed { index, offsetState ->
+                    val current = offsetState.value
+                    val next = subCircles[(index + 1) % subCircles.size].value
+
+                    drawLine(
+                        color = Color.Gray,
+                        start = current,
+                        end = next,
+                        strokeWidth = 5f
+                    )
+
+                    val movingCirclePos = Offset(
+                        lerpAction(current.x, next.x, animationProgress),
+                        lerpAction(current.y, next.y, animationProgress)
+                    )
+
+                    drawCircle(
+                        color = Color.Black,
+                        radius = 10f,
+                        center = movingCirclePos
+                    )
+
+                    drawCircle(
+                        brush = circleColors[index],
+                        radius = subCircleRadius.toPx(),
+                        center = current
+                    )
+                }
+            }
+        }
+
+        if (showSubMenu) {
+            subCircles.forEachIndexed { index, offsetState ->
+                val boxSizeDp = subCircleRadius * 2
+
+                Box(
+                    modifier = Modifier
+                        .offset {
+                            IntOffset(
+                                (offsetState.value.x - with(density) { boxSizeDp.toPx() } * 5).roundToInt(),
+                                (offsetState.value.y - with(density) { boxSizeDp.toPx() } * 10).roundToInt()
+                            )
+                        }
+                        .size(boxSizeDp)
+                        .pointerInput(index) {
+                            detectDragGestures { _, dragAmount ->
+                                val newX = (offsetState.value.x + dragAmount.x).coerceIn(
+                                    0f,
+                                    center.value.x * 2 - subCircleRadius.toPx()
+                                )
+                                val newY = (offsetState.value.y + dragAmount.y).coerceIn(
+                                    0f,
+                                    center.value.y * 2 - subCircleRadius.toPx()
+                                )
+                                offsetState.value = Offset(newX, newY)
+                            }
+                        }
+                )
+            }
+        }
+
+        Text("Main Menu", color = Color.White, modifier = Modifier.align(Alignment.Center))
+
+        Box(
+            modifier = Modifier
+                .size(mainCircleRadius * 2)
+                .align(Alignment.Center)
+                .pointerInput(Unit) {
+                    detectTapGestures {
+                        showSubMenu = !showSubMenu
+                        if (showSubMenu) {
+                            for (i in 1..6) {
+                                val angle = 60 * i
+                                val x =
+                                    center.value.x + cos(Math.toRadians(angle.toDouble())) * distanceFromCenter.toPx()
+                                val y =
+                                    center.value.y + sin(Math.toRadians(angle.toDouble())) * distanceFromCenter.toPx()
+                                subCircles[i - 1].value = Offset(x.toFloat(), y.toFloat())
+                            }
+                        }
+                    }
+                }
+        )
+    }
+}
+
+fun lerpAction(start: Float, stop: Float, fraction: Float): Float = (1 - fraction) * start + fraction * stop*/
+
 
 /*var showContent by remember { mutableStateOf(false) }
 var text by remember { mutableStateOf("") }
@@ -145,27 +271,53 @@ modifier = Modifier.padding(top = 8.dp)
 fun WoofApp(text: String) {
     val dogs = listOf(
         Dog(painterResource("koda.jpg"), "Koda", 2, "Eating treats on the terrace"),
+        Dog(painterResource("koda.jpg"), "Koda", 2, "Eating treats on the terrace"),
+        Dog(painterResource("koda.jpg"), "Koda", 2, "Eating treats on the terrace"),
+        Dog(painterResource("koda.jpg"), "Koda", 2, "Eating treats on the terrace"),
+        Dog(painterResource("lola.jpg"), "Lola", 16, "Barking at Daddy"),
+        Dog(painterResource("lola.jpg"), "Lola", 16, "Barking at Daddy"),
+        Dog(painterResource("lola.jpg"), "Lola", 16, "Barking at Daddy"),
+        Dog(painterResource("lola.jpg"), "Lola", 16, "Barking at Daddy"),
+        Dog(painterResource("lola.jpg"), "Lola", 16, "Barking at Daddy"),
         Dog(painterResource("lola.jpg"), "Lola", 16, "Barking at Daddy"),
         Dog(painterResource("frankie.jpg"), "Frankie", 2, "Stealing socks"),
+        Dog(painterResource("frankie.jpg"), "Frankie", 2, "Stealing socks"),
+        Dog(painterResource("frankie.jpg"), "Frankie", 2, "Stealing socks"),
+        Dog(painterResource("frankie.jpg"), "Frankie", 2, "Stealing socks"),
+        Dog(painterResource("frankie.jpg"), "Frankie", 2, "Stealing socks"),
+        Dog(painterResource("frankie.jpg"), "Frankie", 2, "Stealing socks"),
         Dog(painterResource("nox.jpg"), "Nox", 8, "Meeting new animals"),
+        Dog(painterResource("nox.jpg"), "Nox", 8, "Meeting new animals"),
+        Dog(painterResource("nox.jpg"), "Nox", 8, "Meeting new animals"),
+        Dog(painterResource("nox.jpg"), "Nox", 8, "Meeting new animals"),
+        Dog(painterResource("faye.jpg"), "Faye", 8, "Digging in the garden"),
+        Dog(painterResource("faye.jpg"), "Faye", 8, "Digging in the garden"),
+        Dog(painterResource("faye.jpg"), "Faye", 8, "Digging in the garden"),
+        Dog(painterResource("faye.jpg"), "Faye", 8, "Digging in the garden"),
         Dog(painterResource("faye.jpg"), "Faye", 8, "Digging in the garden"),
         Dog(painterResource("bella.jpg"), "Bella", 14, "Chasing sea foam"),
         Dog(painterResource("moana.jpg"), "Moana", 2, "Bothering her paw-rents"),
         Dog(painterResource("tzeitel.jpg"), "Tzeitel", 7, "Sunbathing"),
         Dog(painterResource("leroy.jpg"), "Leroy", 4, "Sleeping in dangerous places")
     )
+    val grouped = dogs.groupBy { it.name[0] }
 
-    if (text.isEmpty()){
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(minSize = 400.dp)
+    if (text.isEmpty()) {
+        LazyColumn(
+
         ) {
+            grouped.forEach { (initial, dogsForInitial) ->
+                stickyHeader {
+                    CharacterHeader(initial)
+                }
 
-            items(dogs) {
-                DogItem(
-                    dog = it,
-                    text,
-                    modifier = Modifier.padding(8.dp).animateItemPlacement()
-                )
+                items(dogsForInitial) {
+                    DogItem(
+                        dog = it,
+                        text,
+                        modifier = Modifier.padding(8.dp).animateItemPlacement()
+                    )
+                }
             }
         }
     } else {
@@ -184,6 +336,13 @@ fun WoofApp(text: String) {
         }
     }
 
+}
+
+@Composable
+fun CharacterHeader(initial: Char) {
+    Box(Modifier.fillMaxWidth().padding(horizontal = 8.dp).background(Color.LightGray)) {
+        Text(initial.toString(), color = Color(0xFF8798d2), modifier = Modifier.padding(start = 12.dp))
+    }
 }
 
 @Composable
@@ -259,8 +418,8 @@ private fun DogItemButton(
 
 @Composable
 fun DogIcon(
-   dogIcon: Painter,
-   expanded: Boolean,
+    dogIcon: Painter,
+    expanded: Boolean,
     modifier: Modifier = Modifier
 ) {
     val infiniteTransition = rememberInfiniteTransition()
@@ -271,7 +430,7 @@ fun DogIcon(
             animation = tween(2000, easing = LinearEasing)
         )
     )
-    if (expanded){
+    if (expanded) {
         Image(
             modifier = modifier
                 .size(64.dp)
@@ -302,7 +461,7 @@ fun DogIcon(
 fun DogInformation(
     dogName: String,
     dogAge: Int,
-    text:String,
+    text: String,
     modifier: Modifier = Modifier
 ) {
 
@@ -322,7 +481,7 @@ fun DogInformation(
         } else {
             BasicText(
                 text = dogName,
-                color = {animatedColor},
+                color = { animatedColor },
                 modifier = Modifier.padding(top = 8.dp),
                 style = TextStyle(fontSize = 21.sp, fontWeight = FontWeight.Bold)
             )
