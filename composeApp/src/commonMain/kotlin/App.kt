94de25db37b37ff9fa617f735544211daa3a6aa9
Muhammad Atif <ma2669637@gmail.com>
2024-02-19 18:08:50 +0500

    updated resources

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -24,19 +25,28 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.Card
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.materialIcon
 import androidx.compose.material.icons.materialPath
+import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -48,9 +58,9 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinproject.composeapp.generated.resources.Res
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
-import kotlin.math.*
 
 
 @OptIn(ExperimentalResourceApi::class)
@@ -74,231 +84,23 @@ fun App() {
     }
 }
 
-/*@Composable
-fun MainMenuCanvas() {
-    var showSubMenu by remember { mutableStateOf(false) }
-    val mainCircleRadius = 50.dp
-    val subCircleRadius = 20.dp
-    val distanceFromCenter = 100.dp
-    val subCircles = remember { List(6) { mutableStateOf(Offset.Zero) } }
-    val density = LocalDensity.current
 
-    val animationProgress by animateFloatAsState(
-        targetValue = if (showSubMenu) 1f else 0f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(durationMillis = 2000, easing = LinearEasing),
-            repeatMode = RepeatMode.Restart
-        ), label = ""
-    )
-
-    val circleColors = remember {
-        List(6) {
-            Brush.linearGradient(
-                colors = listOf(
-                    Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f),
-                    Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f)
-                )
-            )
-        }
-    }
-
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.LightGray)
-    ) {
-        val center = remember { mutableStateOf(Offset.Zero) }
-
-        Canvas(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            center.value = Offset(size.width / 2, size.height / 2)
-
-            drawCircle(color = Color.Blue, radius = mainCircleRadius.toPx(), center = center.value)
-
-            if (showSubMenu) {
-                subCircles.forEachIndexed { index, offsetState ->
-                    val current = offsetState.value
-                    val next = subCircles[(index + 1) % subCircles.size].value
-
-                    drawLine(
-                        color = Color.Gray,
-                        start = current,
-                        end = next,
-                        strokeWidth = 5f
-                    )
-
-                    val movingCirclePos = Offset(
-                        lerpAction(current.x, next.x, animationProgress),
-                        lerpAction(current.y, next.y, animationProgress)
-                    )
-
-                    drawCircle(
-                        color = Color.Black,
-                        radius = 10f,
-                        center = movingCirclePos
-                    )
-
-                    drawCircle(
-                        brush = circleColors[index],
-                        radius = subCircleRadius.toPx(),
-                        center = current
-                    )
-                }
-            }
-        }
-
-        if (showSubMenu) {
-            subCircles.forEachIndexed { index, offsetState ->
-                val boxSizeDp = subCircleRadius * 2
-
-                Box(
-                    modifier = Modifier
-                        .offset {
-                            IntOffset(
-                                (offsetState.value.x - with(density) { boxSizeDp.toPx() } * 5).roundToInt(),
-                                (offsetState.value.y - with(density) { boxSizeDp.toPx() } * 10).roundToInt()
-                            )
-                        }
-                        .size(boxSizeDp)
-                        .pointerInput(index) {
-                            detectDragGestures { _, dragAmount ->
-                                val newX = (offsetState.value.x + dragAmount.x).coerceIn(
-                                    0f,
-                                    center.value.x * 2 - subCircleRadius.toPx()
-                                )
-                                val newY = (offsetState.value.y + dragAmount.y).coerceIn(
-                                    0f,
-                                    center.value.y * 2 - subCircleRadius.toPx()
-                                )
-                                offsetState.value = Offset(newX, newY)
-                            }
-                        }
-                )
-            }
-        }
-
-        Text("Main Menu", color = Color.White, modifier = Modifier.align(Alignment.Center))
-
-        Box(
-            modifier = Modifier
-                .size(mainCircleRadius * 2)
-                .align(Alignment.Center)
-                .pointerInput(Unit) {
-                    detectTapGestures {
-                        showSubMenu = !showSubMenu
-                        if (showSubMenu) {
-                            for (i in 1..6) {
-                                val angle = 60 * i
-                                val x =
-                                    center.value.x + cos(Math.toRadians(angle.toDouble())) * distanceFromCenter.toPx()
-                                val y =
-                                    center.value.y + sin(Math.toRadians(angle.toDouble())) * distanceFromCenter.toPx()
-                                subCircles[i - 1].value = Offset(x.toFloat(), y.toFloat())
-                            }
-                        }
-                    }
-                }
-        )
-    }
-}
-
-fun lerpAction(start: Float, stop: Float, fraction: Float): Float = (1 - fraction) * start + fraction * stop*/
-
-
-/*var showContent by remember { mutableStateOf(false) }
-var text by remember { mutableStateOf("") }
-val greeting = remember { Greeting().greet() }
-Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-    OutlinedTextField(
-        value = text,
-        onValueChange = { text = it },
-        modifier = Modifier.padding(top = 100.dp),
-        label = { Text("Please enter some value") }
-
-    )
-    Button(onClick = { showContent = !showContent }, modifier = Modifier.padding(top = 18.dp)) {
-        Text("Click me!")
-    }
-
-    Text(
-        buildAnnotatedString {
-            withStyle(
-                style = SpanStyle(
-                    fontWeight = FontWeight.Bold, color = Color.Magenta,
-                    textDecoration = TextDecoration.Underline
-                )
-            ) {
-                append(text.take(1))
-            }
-            append(text.drop(1))
-        },
-        fontSize = 81.sp,
-        fontWeight = FontWeight.Bold,
-        modifier = Modifier.padding(16.dp)
-    )
-}*/
-
-/////////////////////////  Annotated String Builder Code ///////////////////
-/*Text(
-text = buildAnnotatedString {
-    val startIndex = dogName.indexOf(text, ignoreCase = true)
-    if (startIndex != -1) {
-        append(dogName.substring(0, startIndex))
-        withStyle(
-            style = SpanStyle(
-                fontWeight = FontWeight.Bold,
-                color = Color.Magenta,
-                textDecoration = TextDecoration.Underline
-            )
-        ) {
-            append(dogName.substring(startIndex, startIndex + text.length))
-        }
-        append(dogName.substring(startIndex + text.length))
-    } else {
-        append(dogName)
-    }
-},
-modifier = Modifier.padding(top = 8.dp)
-)*/
-
-/////////////////////////  End   ///////////////////
-
-@OptIn(ExperimentalResourceApi::class, ExperimentalFoundationApi::class)
+@OptIn(
+    ExperimentalResourceApi::class, ExperimentalFoundationApi::class,
+    ExperimentalMaterialApi::class
+)
 @Composable
 fun WoofApp(text: String) {
-    val dogs = listOf(
-        Dog(painterResource("koda.jpg"), "Koda", 2, "Eating treats on the terrace"),
-        Dog(painterResource("koda.jpg"), "Koda", 2, "Eating treats on the terrace"),
-        Dog(painterResource("koda.jpg"), "Koda", 2, "Eating treats on the terrace"),
-        Dog(painterResource("koda.jpg"), "Koda", 2, "Eating treats on the terrace"),
-        Dog(painterResource("lola.jpg"), "Lola", 16, "Barking at Daddy"),
-        Dog(painterResource("lola.jpg"), "Lola", 16, "Barking at Daddy"),
-        Dog(painterResource("lola.jpg"), "Lola", 16, "Barking at Daddy"),
-        Dog(painterResource("lola.jpg"), "Lola", 16, "Barking at Daddy"),
-        Dog(painterResource("lola.jpg"), "Lola", 16, "Barking at Daddy"),
-        Dog(painterResource("lola.jpg"), "Lola", 16, "Barking at Daddy"),
-        Dog(painterResource("frankie.jpg"), "Frankie", 2, "Stealing socks"),
-        Dog(painterResource("frankie.jpg"), "Frankie", 2, "Stealing socks"),
-        Dog(painterResource("frankie.jpg"), "Frankie", 2, "Stealing socks"),
-        Dog(painterResource("frankie.jpg"), "Frankie", 2, "Stealing socks"),
-        Dog(painterResource("frankie.jpg"), "Frankie", 2, "Stealing socks"),
-        Dog(painterResource("frankie.jpg"), "Frankie", 2, "Stealing socks"),
-        Dog(painterResource("nox.jpg"), "Nox", 8, "Meeting new animals"),
-        Dog(painterResource("nox.jpg"), "Nox", 8, "Meeting new animals"),
-        Dog(painterResource("nox.jpg"), "Nox", 8, "Meeting new animals"),
-        Dog(painterResource("nox.jpg"), "Nox", 8, "Meeting new animals"),
-        Dog(painterResource("faye.jpg"), "Faye", 8, "Digging in the garden"),
-        Dog(painterResource("faye.jpg"), "Faye", 8, "Digging in the garden"),
-        Dog(painterResource("faye.jpg"), "Faye", 8, "Digging in the garden"),
-        Dog(painterResource("faye.jpg"), "Faye", 8, "Digging in the garden"),
-        Dog(painterResource("faye.jpg"), "Faye", 8, "Digging in the garden"),
-        Dog(painterResource("bella.jpg"), "Bella", 14, "Chasing sea foam"),
-        Dog(painterResource("moana.jpg"), "Moana", 2, "Bothering her paw-rents"),
-        Dog(painterResource("tzeitel.jpg"), "Tzeitel", 7, "Sunbathing"),
-        Dog(painterResource("leroy.jpg"), "Leroy", 4, "Sleeping in dangerous places")
+    val dogs = mutableListOf<Dog>(
+        Dog(painterResource(Res.drawable.koda), "Koda", 2, "Eating treats on the terrace"),
+        Dog(painterResource(Res.drawable.lola), "Lola", 16, "Barking at Daddy"),
+        Dog(painterResource(Res.drawable.frankie), "Frankie", 2, "Stealing socks"),
+        Dog(painterResource(Res.drawable.nox), "Nox", 8, "Meeting new animals"),
+        Dog(painterResource(Res.drawable.faye), "Faye", 8, "Digging in the garden"),
+        Dog(painterResource(Res.drawable.bella), "Bella", 14, "Chasing sea foam"),
+        Dog(painterResource(Res.drawable.moana), "Moana", 2, "Bothering her paw-rents"),
+        Dog(painterResource(Res.drawable.tzeitel), "Tzeitel", 7, "Sunbathing"),
+        Dog(painterResource(Res.drawable.leroy), "Leroy", 4, "Sleeping in dangerous places")
     )
     val grouped = dogs.groupBy { it.name[0] }
 
@@ -306,19 +108,58 @@ fun WoofApp(text: String) {
         LazyColumn(
 
         ) {
-            grouped.forEach { (initial, dogsForInitial) ->
-                stickyHeader {
-                    CharacterHeader(initial)
-                }
 
-                items(dogsForInitial) {
+            items(dogs) { dog ->
+
+                val state = rememberDismissState(
+                    confirmStateChange = {
+                        if (it == DismissValue.DismissedToStart) {
+                            dogs.remove(dog)
+                        }
+                        true
+                    }
+                )
+                SwipeToDismiss(state = state, dismissThresholds = {FractionalThreshold(0.6f)} ,background = {
+                    val color = when (state.dismissDirection) {
+                        DismissDirection.StartToEnd -> Color(0xFFb59df7)
+                        DismissDirection.EndToStart -> Color(0xFFe7def7)
+                        null -> Color.Transparent
+                    }
+                    Box(
+                        Modifier.fillMaxSize().padding(8.dp).background(
+                            color = color,
+                            shape = RoundedCornerShape(topEnd = 16.dp, bottomStart = 16.dp)
+                        )
+                    ) {
+                        if (state.dismissDirection == DismissDirection.StartToEnd) {
+                            Icon(
+                                Icons.Default.Delete, null,
+                                modifier = Modifier.align(Alignment.CenterStart)
+                                    .padding(start = 18.dp)
+                            )
+                        }
+                        if (state.dismissDirection == DismissDirection.EndToStart) {
+                            Icon(
+                                Icons.Default.ShoppingCart, null,
+                                modifier = Modifier.align(Alignment.CenterEnd).size(45.dp)
+                                    .padding(end = 18.dp)
+                            )
+                        }
+                        /* Icon(Icons.Default.Delete, null,
+                             tint = Color.Gray,
+                             modifier = Modifier.align(Alignment.CenterEnd)
+                             )*/
+                    }
+                }, dismissContent = {
                     DogItem(
-                        dog = it,
+                        dog = dog,
                         text,
                         modifier = Modifier.padding(8.dp).animateItemPlacement()
                     )
-                }
+                }, directions = setOf(DismissDirection.EndToStart, DismissDirection.StartToEnd))
+
             }
+
         }
     } else {
         LazyColumn() {
@@ -341,7 +182,11 @@ fun WoofApp(text: String) {
 @Composable
 fun CharacterHeader(initial: Char) {
     Box(Modifier.fillMaxWidth().padding(horizontal = 8.dp).background(Color.LightGray)) {
-        Text(initial.toString(), color = Color(0xFF8798d2), modifier = Modifier.padding(start = 12.dp))
+        Text(
+            initial.toString(),
+            color = Color(0xFF8798d2),
+            modifier = Modifier.padding(start = 12.dp)
+        )
     }
 }
 
